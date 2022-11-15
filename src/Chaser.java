423f1394265971bab8650468d5e857213799c540
Andrew162534 <frauendorfer-andreas@t-online.de3>
2022-11-15 12:27:03 -0800

    Base

---
+++ src/Chaser.java
@@ -0,0 +1,29 @@
+public abstract class Chaser implements Comparable {
+    private int[] position = new int[2];
+    private String name;
+
+    //default constructor?
+
+    public abstract int[] nextMove();
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(int[] position) {
+        this.position = position;
+    }
+
+    @Override       // TODO
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,8 @@
+public class Game {
+    private final int FIELD_SIZE = 8;
+    private int[][] field = new int[FIELD_SIZE][FIELD_SIZE];
+
+    public static void main(String[] args) {
+
+    }
+}
