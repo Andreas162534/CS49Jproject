aa9328518912916499009233db1f678b0a20f817
Andrew162534 <frauendorfer-andreas@t-online.de3>
2022-11-15 12:46:51 -0800

    all set

---
+++ src/AndreasChaser.java
@@ -0,0 +1,7 @@
+public class AndreasChaser extends Chaser{
+    @Override
+    public int[] nextMove() {
+        return new int[0];
+    }
+
+}
---
+++ src/AndreasClass.java
@@ -0,0 +1,8 @@
+public class AndreasClass extends AndreasChaser implements Move {
+    @Override
+    public void printPosition(boolean chaser) {
+
+    }
+
+    //can methoden von AndreasChaser benutzen, könnte auch import ausreichen?
+}
