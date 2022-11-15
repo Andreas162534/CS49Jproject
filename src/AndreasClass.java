e83ff9ab790ed62e6f341ec8977528212dd46ad2
Andrew162534 <frauendorfer-andreas@t-online.de3>
2022-11-15 12:48:23 -0800

    show return

--- src/AndreasClass.java
+++ src/AndreasClass.java
@@ -1,7 +1,7 @@
 public class AndreasClass extends AndreasChaser implements Move {
     @Override
     public void printPosition(boolean chaser) {
-
+        return;
     }
 
     //can methoden von AndreasChaser benutzen, könnte auch import ausreichen?
