6798f3c7c90e4259820842adbc7b52e8f75147c3
RanveerSingh23 <90824237+RanveerSingh23@users.noreply.github.com>
2022-12-06 11:54:14 -0800

    Update RanveerCharacter.java

--- src/RanveerCharacter.java
+++ src/RanveerCharacter.java
@@ -32,11 +32,11 @@ public class RanveerCharacter extends Character{
      */
     @Override
     public void nextMove(int[]... dir) { // will move at random between the board with no preset patter // WILDCARD
-        int rngX = getRandomNumber(0,8);
-        int rngY = getRandomNumber(0,8);
+        int rngX = getRandomNumber(0,7);
+        int rngY = getRandomNumber(0,7);
         int X = this.getPosition()[0]+rngX;
         int Y = this.getPosition()[1]+rngY;
-        if(X == 4 || Y == 4) //if we are in the center of the board get a new position
+        if(X == 4 && Y == 4) //if we are in the center of the board get a new position
             nextMove(new int[]{X, Y});
         this.setPosition(new int[]{X, Y});
     }
