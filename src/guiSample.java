6a7042839f7a49981051138d76b9b6463233b4fb
RanveerSingh23 <90824237+RanveerSingh23@users.noreply.github.com>
2022-12-05 21:59:37 -0800

    Update guiSample.java

--- src/guiSample.java
+++ src/guiSample.java
@@ -1,57 +1,71 @@
-import java.awt.event.*;
-import java.awt.*;
-import javax.swing.*;
-class box extends JFrame implements ActionListener {
-    static JTextField t;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    static JFrame f;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-    static JButton b;
+public class DiscordLike {
+    public static void main(String[] args) {
+        // Create the main frame for the GUI
+        JFrame frame = new JFrame("Discord");
+        frame.setLayout(new BorderLayout());
 
-    static JLabel l;
+        // Create the panel for the chat messages
+        JPanel chatPanel = new JPanel();
+        chatPanel.setLayout(new BorderLayout());
 
-    box()
-    {
-    }
+        // Create the text area for the chat messages
+        JTextArea chatBox = new JTextArea();
+        chatBox.setEditable(false);
+        chatBox.setLineWrap(true);
+        chatBox.setWrapStyleWord(true);
 
-    public static void main(String[] args)
-    {
-        f = new JFrame("Discord");
+        // Add the chat box to a scroll pane to allow scrolling
+        JScrollPane chatScroll = new JScrollPane(chatBox);
+        chatScroll.setPreferredSize(new Dimension(500, 300));
 
-        l = new JLabel("Empty");
+        // Add the chat scroll pane to the chat panel
+        chatPanel.add(chatScroll, BorderLayout.CENTER);
 
-        b = new JButton("submit");
+        // Create the panel for the message input and send button
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
 
-        box te = new box();
+        // Create the text field for the message input
+        JTextField inputField = new JTextField();
+        inputField.setPreferredSize(new Dimension(400, 30));
 
-        b.addActionListener(te);
+        // Create the button for sending messages
+        JButton sendButton = new JButton("Send");
 
-        t = new JTextField(16);
+        // Add the input field and send button to the input panel
+        inputPanel.add(inputField);
+        inputPanel.add(sendButton);
 
-        Font fo = new Font("Serif", Font.BOLD, 20);
+        // Add the chat panel and input panel to the main frame
+        frame.add(chatPanel, BorderLayout.CENTER);
+        frame.add(inputPanel, BorderLayout.SOUTH);
 
-        t.setFont(fo);
+        frame.pack(); // Resize the window to fit the components
+        frame.setVisible(true);
 
-        JPanel p = new JPanel();
-
-        p.add(t);
-        p.add(b);
-        p.add(l);
-
-        f.add(p);
-
-        f.setSize(300, 300);
-
-        f.show();
-    }
-
-    public void actionPerformed(ActionEvent e)
-    {
-        String s = e.getActionCommand();
-        if (s.equals("submit")) {
-            l.setText(t.getText());
-
-            t.setText("  ");
-        }
+        // Add an action listener to the send button
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = inputField.getText();
+                if (!message.isEmpty()) {
+                    chatBox.append(message + "\n");
+                    inputField.setText("");
+                }
+            }
+        });
     }
 }
